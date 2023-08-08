943e55e7ee5d7467f07753def02c86fdc0e4c53e
Jose Saribong <jose.c.saribong@gmail.com>
2023-08-08 20:16:47 +0800

    added default value to account_pending

--- db/migrate/20230808121418_add_account_pending_to_users.rb
+++ db/migrate/20230808121418_add_account_pending_to_users.rb
@@ -1,5 +1,5 @@
 class AddAccountPendingToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :account_pending, :boolean
+    add_column :users, :account_pending, :boolean, default: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_121418) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "account_pending"
+    t.boolean "account_pending", default: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
