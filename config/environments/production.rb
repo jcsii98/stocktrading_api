7aaca747122e6f4208bd16f629d64054079adfb5
Jose Saribong <jose.c.saribong@gmail.com>
2023-08-28 19:18:33 +0800

    mail prod setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,11 +53,10 @@ Rails.application.configure do
 
   # devise setup
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # mailcatcher config
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address: 'localhost',
-  port: 1025, # Default MailCatcher SMTP port
-}
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
