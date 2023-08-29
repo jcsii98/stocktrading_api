bbf77c1004a4d9fae58c29786726aa9a102775cf
Jose Saribong <jose.c.saribong@gmail.com>
2023-08-29 19:55:11 +0800

    modified header

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -24,7 +24,7 @@ class StocksController < ApplicationController
 
                                 },
                             headers: {
-                                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+                                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
                                 })
 
     coin_data = JSON.parse(response.body)
