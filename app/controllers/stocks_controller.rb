e5a17523271dda4af1a0137d0852ef5b7e937909
Jose Saribong <jose.c.saribong@gmail.com>
2023-08-29 20:29:16 +0800

    changed test url

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,45 +9,24 @@ class StocksController < ApplicationController
         render json: { message: 'Stock data updated' }
     end
 
-    # test api call
-    
-    def fetch_coin_data
-    response = RestClient.get("https://api.coingecko.com/api/v3/coins/markets", 
-                            params: {
-                                vs_currency: 'usd',
-                                category: 'aave-tokens',
-                                order: 'market_cap_desc',
-                                per_page: 100,
-                                page: 1,
-                                sparkline: false,
-                                locale: 'en',
-
-                                },
-                            headers: {
-                                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-                                })
-
-    coin_data = JSON.parse(response.body)
-    render json: { coin_data: coin_data }
-#   rescue RestClient::ExceptionWithResponse => e
-#     render json: { error: e.message }, status: e.response.code
-  end
 
     def test_rest
-        response = RestClient.get("https://api.coingecko.com/api/v3/coins/markets", 
-                                params: {
-                                    vs_currency: 'usd',
-                                    category: 'aave-tokens',
-                                    order: 'market_cap_desc',
-                                    per_page: 100,
-                                    page: 1,
-                                    sparkline: false,
-                                    locale: 'en',
+        response = RestClient.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&order=market_cap_desc&per_page=250&page=1&sparkline=false&locale=en", 
+        # response = RestClient.get("https://api.coingecko.com/api/v3/coins/markets", 
+                                # params: {
+                                #     vs_currency: 'usd',
+                                #     category: 'aave-tokens',
+                                #     order: 'market_cap_desc',
+                                #     per_page: 100,
+                                #     page: 1,
+                                #     sparkline: false,
+                                #     locale: 'en',
 
-                                    },
-                                headers: {
-                                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-                                    })
+                                #     },
+                                # headers: {
+                                #         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+                                #     }
+        )
 
         coin_data = JSON.parse(response.body)
         render json: { coin_data: coin_data }
@@ -56,19 +35,13 @@ class StocksController < ApplicationController
     end
 
     def test_party
-        response = HTTParty.get("https://api.coingecko.com/api/v3/coins/markets", 
-                                query: {
-                                    vs_currency: 'usd',
-                                    category: 'aave-tokens',
-                                    order: 'market_cap_desc',
-                                    per_page: 100,
-                                    page: 1,
-                                    sparkline: false,
-                                    locale: 'en'
-                                },
-                                headers: {
-                                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-                                })
+        response = HTTParty.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&order=market_cap_desc&per_page=250&page=1&sparkline=false&locale=en", 
+        # response = HTTParty.get("https://api.coingecko.com/api/v3/coins/markets", 
+                                
+                                # headers: {
+                                #     'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+                                # }
+        )
 
         coin_data = JSON.parse(response.body)
         render json: { coin_data: coin_data }
